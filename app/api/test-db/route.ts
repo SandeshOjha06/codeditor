87fa4f807582a180364c6e2f90fe561523333269
Sandesh <sandeshojha70@gmail.com>
2025-12-19 16:54:05 +0000

    add dependency

--- app/api/test-db/route.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@/src/db";
-import { users } from "@/src/db/schema";
-
-export async function GET() {
-    const user = await db.select().from(users)
-    return new Response(JSON.stringify(user))
-    
-}
